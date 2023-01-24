75d20d0c406c3d887e456dd1fad66142d2ac4f19
crowemi <crowemi@hotmail.com>
2023-01-24 05:57:01 -0800

    updated storage to receive polars df convert to pyarrow

--- storage/base_storage.py
+++ storage/base_storage.py
@@ -1,5 +1,8 @@
 import os
+import logging
 
+from polars import DataFrame
+from pyarrow import Table, Schema
 from pyarrow import fs
 
 from abc import ABCMeta, abstractmethod
@@ -11,6 +14,9 @@ STORAGE_TYPES = [
 
 class BaseStorage(metaclass=ABCMeta):
     def __init__(self, type: str, region: str) -> None:
+
+        self.LOGGER = logging.getLogger('crowemi-trades.storage')
+
         if type not in STORAGE_TYPES:
             raise Exception("Invalid storage type.")
 
@@ -28,3 +34,14 @@ class BaseStorage(metaclass=ABCMeta):
     @abstractmethod
     def read():
         pass
+
+    def create_data_table(self, df: DataFrame) -> tuple[Table, Schema]:
+        """Creates a pyarrow Table object from a polars dataframe."""
+        try:
+            data = Table.from_pandas(df.to_pandas())
+            schema = Table.from_pandas(df.to_pandas()).schema
+            return data, schema
+        except Exception as e:
+            self.LOGGER.error('Failed to create parquet dataframe.')
+            self.LOGGER.exception(e)
+            raise e
--- storage/s3_storage.py
+++ storage/s3_storage.py
@@ -1,5 +1,5 @@
 from polars import DataFrame
-import pyarrow
+
 from pyarrow.parquet import ParquetWriter
 
 from storage.base_storage import BaseStorage
@@ -14,28 +14,15 @@ class S3Storage(BaseStorage):
     def write(self, key: str, df: DataFrame) -> None:
         try:
             compression = 'gzip' # TODO: support multiple compression types
-            table = self.create_data_table(df)
+            data, schema = self.create_data_table(df)
             ParquetWriter(
                 f"{key}.parquet.{compression}",
-                df.schema,
+                schema,
                 compression=compression,
                 filesystem=self.file_system,
-            ).write_table(df)
-            self.file_iterator += 1
+            ).write_table(data)
         except Exception as e:
-            self.logger.error('Failed to write parquet file to S3.')
+            self.LOGGER.error('Failed to write parquet file to S3.')
+            self.LOGGER.exception(e)
             raise
 
-    def create_data_table(self, df: DataFrame) -> pyarrow.Table:
-        """Creates a pyarrow Table object from the record set."""
-        try:
-            fields = set()
-            for d in df.records:
-                fields = fields.union(d.keys())
-            dataframe = pyarrow.table({f: [row.get(f) for row in self.records] for f in fields})
-        except Exception as e:
-            self.logger.error('Failed to create parquet dataframe.')
-            self.logger.error(e)
-            raise e
-
-        return dataframe
