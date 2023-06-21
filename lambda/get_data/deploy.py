8a4c49095406c8da8e0f173055f7e4902b2db8ce
crowemi <crowemi@hotmail.com>
2023-06-21 06:44:58 -0700

    update deploy script

--- lambda/get_data/deploy.py
+++ lambda/get_data/deploy.py
@@ -5,7 +5,7 @@ import botocore
 
 function_name = "crowemi-trades-lambda-get-data"
 role = "arn:aws:iam::926488920335:role/crowemi-trades-lambda-get-data"
-image_uri = f"{os.getenv('ECR_REGISTRY')}/{os.getenv('ECR_REPOSITORY')}:{os.getenv('IMAGE_TAG')}"
+image_uri = f"{os.getenv('ECR_REGISTRY', '926488920335.dkr.ecr.us-west-2.amazonaws.com')}/{os.getenv('ECR_REPOSITORY', 'crowemi-trades-lambda-get-data')}:{os.getenv('IMAGE_TAG', 'latest')}"
 polygon_key = os.getenv("POLYGON_KEY")
 mongodb_uri = os.getenv("MONGODB_URI")
 
@@ -24,10 +24,19 @@ definition = {
 
 try:
     response = client.get_function(FunctionName="crowemi-trades-lambda-get-data")
+    # update code
     response = client.update_function_code(
         FunctionName=function_name,
         ImageUri=image_uri,
         Publish=True,
+    )
+    # wait for function to finish updating
+    waiter = client.get_waiter("function_updated")
+    waiter.wait(FunctionName="crowemi-trades-lambda-get-data")
+    # update configuration
+    response = client.update_function_configuration(
+        FunctionName=definition["FunctionName"],
+        Environment=definition["Environment"],
     )
     print(response)
 except botocore.exceptions.ClientError as e:
